8edcd818a5b322fd6b83cb676c422662f99f198e
gh447 <101194476+gh447@users.noreply.github.com>
2022-03-08 23:03:53 +0800

    Add files via upload

---
+++ FloatCalc.java
@@ -0,0 +1,9 @@
+public class FloatCalc {
+    public static void main(String[] args){
+        System.out.println(1>2);
+        System.out.println(1<2);
+
+        System.out.println("ABC");
+        System.out.println('A');
+    }
+}
---
+++ PrimaryTypes.java
@@ -0,0 +1,32 @@
+public class PrimaryTypes {
+    public static void main(String[] args) {
+        byte byteVar = 99;
+        System.out.println(byteVar);
+
+        short shortVar = -30000;
+        System.out.println(shortVar);
+
+        int intVar = 300000;
+        System.out.println(intVar);
+
+        long longVar = 99;
+
+        long biglongVar = 99999999999L;
+        System.out.println(biglongVar);
+
+        float floatVar = 100.111111111f;
+        System.out.println(floatVar);
+
+        double doubleVar = 100.111111111;
+        System.out.println(doubleVar);
+
+        boolean condition = true;
+        boolean fcondition = false;
+        System.out.println(condition);
+        System.out.println(fcondition);
+
+        char ch ='A';
+        System.out.println(ch);
+
+    }
+}
