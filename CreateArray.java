6537638e01e0171545ee43b8c8133c1b960deca7
gh447 <101194476+gh447@users.noreply.github.com>
2022-03-08 23:23:00 +0800

    Add files via upload

---
+++ CreateArray.java
@@ -0,0 +1,13 @@
+public class CreateArray {
+    public static void main(String[] args) {
+        //创建一个数量为88的数组
+        int[] myIntArray = new int [88];
+
+        System.out.println(myIntArray[2]);
+        System.out.println(myIntArray[4]);
+
+        double[] myDoubleArray = new double[77];
+        System.out.println(myDoubleArray[6]);
+        System.out.println(myDoubleArray[76]);
+    }
+}
