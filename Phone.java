cd4cc9938858d44f5434e0bd5baab16aab1cb7ca
gh447 <101194476+gh447@users.noreply.github.com>
2022-03-09 20:40:32 +0800

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,13 @@
+package com.phone;
+
+import com.phone.part.MainBoard;
+import com.phone.part.Screen;
+
+public class Phone {
+    public Screen screen;
+    public MainBoard mainBoard;
+    public double price;
+    public boolean hasFihurePrintUnlcoker;
+
+    public Phone prePhone;
+}
