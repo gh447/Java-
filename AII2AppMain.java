05855e9569d20585c5b600d9d831ce227f6e6745
gh447 <101194476+gh447@users.noreply.github.com>
2022-03-21 23:09:32 +0800

    Add files via upload

---
+++ AII2AppMain.java
@@ -0,0 +1,20 @@
+package com.geekbang.learn.ai;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class AII2AppMain {
+    public static void main(String[] args) {
+        AI2 ai2 = new AI2();
+        Scanner in = new Scanner(System.in);
+        while(true){
+            String input = in.next();
+            if ("exit".equals(input)){
+                System.out.println("再见");
+                break;
+            }
+            String answer = ai2.answer(input);
+            System.out.println(answer);
+        }
+    }
+}
---
+++ LearnString.java
@@ -0,0 +1,28 @@
+package com.geekbang.learn;
+
+import java.util.Locale;
+
+public class LearnString {
+    public static void main(String[] args) {
+        String content = "01234567ABCDefgh";
+
+        //String的length()是个方法不是属性
+        System.out.println(content.length());
+
+        //其实是生成了一个新的String对象
+        System.out.println(content.toUpperCase());
+
+        System.out.println(content.toLowerCase());
+
+        //content指向对象的内容并没有改变
+        System.out.println(content);
+
+        System.out.println(content.charAt(1));
+
+        System.out.println(content.charAt(99));
+
+        System.out.println(content.substring(5));
+
+        System.out.println(content.substring(1,5));
+    }
+}
